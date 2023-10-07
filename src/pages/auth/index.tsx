a65f8874ff573b4b74e8ccf282a5244b42503d2d
zhenyuWang <13641039885@163.com>
2023-10-07 15:27:24 +0800

    chore: init

---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Navbar from '../../../components/Navbar/Navbar'
+
+type authPageProps = {}
+
+const authPage: React.FC<authPageProps> = () => {
+  return <h1>Auth page</h1>
+}
+export default authPage
