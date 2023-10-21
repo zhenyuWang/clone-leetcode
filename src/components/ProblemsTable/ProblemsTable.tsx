f7b55cf3d2b3d61fa25d0e1ddbf8da88e8daa949
zhenyuWang <13641039885@163.com>
2023-10-21 16:07:08 +0800

    perf: problem list add serial number

--- src/components/ProblemsTable/ProblemsTable.tsx
+++ src/components/ProblemsTable/ProblemsTable.tsx
@@ -34,27 +34,27 @@ const ProblemsTable: React.FC<ProblemsTableProps> = ({ setLoadingProblems }) => 
   }, [])
   return (
     <>
-      <div className="text-white">
-        {problems.map((problem, idx) => {
+      <div className='text-white'>
+        {problems.map((problem, index) => {
           const difficultyColor =
-            problem.difficulty === "Easy"
-              ? "text-dark-green-s"
-              : problem.difficulty === "Medium"
-              ? "text-dark-yellow"
-              : "text-dark-pink"
+            problem.difficulty === 'Easy'
+              ? 'text-dark-green-s'
+              : problem.difficulty === 'Medium'
+              ? 'text-dark-yellow'
+              : 'text-dark-pink'
           return (
-            <div className={`${idx % 2 == 1 ? 'flex bg-dark-layer-1' : 'flex'}`} key={problem.id}>
+            <div className={`${index % 2 == 1 ? 'flex bg-dark-layer-1' : 'flex'}`} key={problem.id}>
               <div className='px-2 py-4 w-[10%] font-medium whitespace-nowrap text-dark-green-s'>
                 {solvedProblems.includes(problem.id) && <BsCheckCircle fontSize={'18'} width='18' />}
               </div>
               <div className='px-1 py-4 w-[38%]'>
                 {problem.link ? (
                   <Link href={problem.link} className='hover:text-blue-600 cursor-pointer' target='_blank'>
-                    {problem.title}
+                    {index + 1}. {problem.title}
                   </Link>
                 ) : (
                   <Link className='hover:text-blue-600 cursor-pointer' href={`/problems/${problem.id}`}>
-                    {problem.title}
+                    {index + 1}. {problem.title}
                   </Link>
                 )}
               </div>
@@ -76,13 +76,13 @@ const ProblemsTable: React.FC<ProblemsTableProps> = ({ setLoadingProblems }) => 
         })}
       </div>
       {youtubePlayer.isOpen && (
-        <div className="fixed top-0 left-0 h-screen w-screen flex items-center justify-center">
-          <div className="bg-black z-10 opacity-70 top-0 left-0 w-screen h-screen absolute" onClick={closeModal}></div>
-          <div className="w-full z-50 h-full px-6 relative max-w-4xl">
-            <div className="w-full h-full flex items-center justify-center relative">
-              <div className="w-full relative">
-                <IoClose fontSize={"35"} className="cursor-pointer absolute -top-16 right-0" onClick={closeModal} />
-                <YouTube videoId={youtubePlayer.videoId} loading="lazy" iframeClassName="w-full min-h-[500px]" />
+        <div className='fixed top-0 left-0 h-screen w-screen flex items-center justify-center'>
+          <div className='bg-black z-10 opacity-70 top-0 left-0 w-screen h-screen absolute' onClick={closeModal}></div>
+          <div className='w-full z-50 h-full px-6 relative max-w-4xl'>
+            <div className='w-full h-full flex items-center justify-center relative'>
+              <div className='w-full relative'>
+                <IoClose fontSize={'35'} className='cursor-pointer absolute -top-16 right-0' onClick={closeModal} />
+                <YouTube videoId={youtubePlayer.videoId} loading='lazy' iframeClassName='w-full min-h-[500px]' />
               </div>
             </div>
           </div>
